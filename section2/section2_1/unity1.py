bca8bb4590c13bf7117cdb3cd76fb616611b4217
messinisk <messiniskostas0@gmail.com>
2024-04-09 06:29:32 +0300

    update

--- section2/section2_1/unity1.py
+++ section2/section2_1/unity1.py
@@ -1,4 +1,4 @@
-print("To 5 είναι int", isinstance(5, int))
+print("To 5 είναι int;", isinstance(5, int))
 #To 5 είναι int True
-print("To 5 είναι str", isinstance(5, str))
+print("To 5 είναι str;", isinstance(5, str))
 #To 5 είναι str False
